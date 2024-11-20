3a47d99b2dffce82ed6dd3b7724587cef3775006
Harun Karal <hrnkral@gmail.com>
2024-11-20 18:10:23 +0300

    baslangic fonksiyonu yapildi

---
+++ baslangic.js
@@ -0,0 +1,13 @@
+function sayiUret()
+{
+    return Math.floor(Math.random() * 90);
+}
+sayiUret(); // Return number between 1-90
+var sayi = sayiUret();
+var sayi2 = sayiUret();
+var sayi3 = sayiUret();
+var sayi4 = sayiUret();
+var sayi5 = sayiUret();
+var sayi6 = sayiUret();
+
+console.log('Kolon :'+' '+sayi+' '+sayi2+' '+sayi3+' '+sayi4+' '+sayi5+' '+sayi6);
