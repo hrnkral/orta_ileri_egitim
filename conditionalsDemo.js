bdf9a6612c24211914aea97cb60073b58f407193
Harun Karal <hrnkral@gmail.com>
2024-11-20 18:46:16 +0300

    function to find the largest number

---
+++ conditionalsDemo.js
@@ -0,0 +1,17 @@
+var sayi1 = 10;
+var sayi2 = 30;
+var sayi3 = 15;
+
+var enBuyuk = sayi1;
+
+if (sayi2 > enBuyuk)
+{
+    enBuyuk = sayi2;
+}
+
+if (sayi3 > enBuyuk)
+{
+    enBuyuk = sayi3;
+}
+
+console.log('En büyük sayı :'+' '+enBuyuk); // Output: 30
