2f4d2e92912532e94ed6bd7e9e57a99dd8c131c4
Harun Karal <hrnkral@gmail.com>
2024-11-20 20:18:43 +0300

    while loop examples

---
+++ while.js
@@ -0,0 +1,34 @@
+
+// var sayi = 10;
+// while(sayi > 0){
+//     console.log(sayi);
+//     sayi--;
+// }  
+// output: 10 9 8 7 6 5 4 3 2 1
+
+
+
+// var sayi = 1;
+// while(sayi <= 10){
+
+//     console.log(sayi);
+//     sayi++;
+// }
+// output: 1 2 3 4 5 6 7 8 9 10
+
+// while(sayi <= 10){
+//     if(sayi % 2 == 0){
+//         console.log(sayi);
+//     }
+//     sayi++;
+// }
+// output: 2 4 6 8 10
+
+var sayi = 1;
+while(sayi <= 10){
+    if(sayi % 2 == 1){
+        console.log(sayi);
+    }
+    sayi++;
+}
+// output: 1 3 5 7 9
