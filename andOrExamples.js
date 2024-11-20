d2b982a6091762c42a792b115013ab921923cbc9
Harun Karal <hrnkral@gmail.com>
2024-11-20 19:46:15 +0300

    and or examples were mades

---
+++ andOrExamples.js
@@ -0,0 +1,27 @@
+var sayi1 = 10;
+var sayi2 = 20;
+var sayi3 = 10;
+
+// if (sayi1 == sayi2 && sayi1 == sayi3)
+// {
+//     console.log('Üç sayı da birbirine eşittir.');
+// }
+// else if (sayi1 == sayi2 && sayi2 != sayi3)
+//     {
+//         console.log('Sayı1 ve Sayı2 birbirine eşittir.');
+//     }
+// else if (sayi1 == sayi3 && sayi1 != sayi2)
+//     {
+//         console.log('Sayı1 ve Sayı3 birbirine eşittir.');
+//     }
+// else {
+//     console.log('Sayılar birbirinden farklıdır.');
+// }
+
+if (sayi1 == sayi2 || sayi1 == sayi3)
+{
+    console.log('En az iki sayı birbirine eşittir.');
+}
+else {
+    console.log('Sayılar birbirinden farklıdır.');
+}
