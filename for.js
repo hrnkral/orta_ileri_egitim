659911b48009f823bf819590f57da33bd64df060
Harun Karal <hrnkral@gmail.com>
2024-11-20 20:42:58 +0300

    for ornekleri yapildi

---
+++ for.js
@@ -0,0 +1,24 @@
+/*
+for(var i=0; i<=5; i++) {
+  console.log(i);
+}
+*/
+// output  0 1 2 3 4 5
+
+/*
+for(var i=0; i<=50; i+=2) {
+  console.log(i);
+}
+*/
+// output  0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50
+/*
+for(var i=10; i>=0; i--) {
+  console.log(i);
+}
+*/
+// output  10 9 8 7 6 5 4 3 2 1 0
+
+for(var i=50; i>=0; i-=5) {
+  console.log(i);
+}
+// output  50 45 40 35 30 25 20 15 10 5 0
